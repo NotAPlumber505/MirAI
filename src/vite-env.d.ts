b60e9fd7e8b5f600b3984b84a4ff6c17a85d8687
Mario Casas <MCasas548@gmail.com>
2025-11-08 13:07:28 -0500

    Added env property to import meta

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PLANT_ID_API_KEY: string
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_KEY: string
+  readonly PORT?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
