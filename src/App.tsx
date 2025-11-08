0687c99d84e004e6a2c08f701b71db04e758c691
Saneec <crisman2005@gmail.com>
2025-11-08 01:54:44 -0500

    Refactored Home.tsx to support layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path= "/" element={<Navbar />}>
+          {/*/ Include pages that should include the navbar in here/*/}
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
